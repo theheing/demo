63e87307a4c6bd0295d8b21d67a3db0be2a8d9e9
theheing <theheing@hotmail.com>
2019-04-10 14:10:22 +0700

    add rpt project

---
+++ App_Code/Helper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Globalization;
+
+using System.IO;
+using System.Net.Mail;
+using System.Web.UI;
+//using System.Linq;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Net.Configuration;
+
+
+namespace Automate.Utilities
+{
+    public static partial class Helpers
+    {
+        private static IFormatProvider enCulture = new CultureInfo("en-US");
+        private static IFormatProvider thCulture = new CultureInfo("th-TH");
+        //private string reportServer = ConfigurationManager.AppSettings["ReportServer"];
+        //private string reportDatabase = ConfigurationManager.AppSettings["ReportDatabase"];
+        //private string reportUserName = ConfigurationManager.AppSettings["ReportUserName"];
+        //private string reportPassword = ConfigurationManager.AppSettings["ReportPassword"];
+
+
+        public static IFormatProvider ENCulture
+        {
+            get { return enCulture; }
+        }
+        public static IFormatProvider THCulture
+        {
+            get { return thCulture; }
+        }
+        public static string ReportServer
+        {
+            get { return ConfigurationManager.AppSettings["ReportServer"]; }
+        }
+        public static string ReportDatabase
+        {
+            get { return ConfigurationManager.AppSettings["ReportDatabase"]; }
+        }
+        public static string ReportUserName
+        {
+            get { return ConfigurationManager.AppSettings["ReportUserName"]; }
+        }
+        public static string ReportPassword
+        {
+            get { return ConfigurationManager.AppSettings["ReportPassword"]; }
+        }
+
+        public static bool SendHTMLMail(MailMessage msg)
+        {
+            //Configuration configurationFile = WebConfigurationManager.OpenWebConfiguration(HttpContext.Current.Server.MapPath("~/web.config"));
+            //Configuration configurationFile = WebConfigurationManager.OpenWebConfiguration(HttpContext.Current.Server.MapPath("~/web.config"));
+            //MailSettingsSectionGroup mailSettings = configurationFile.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+
+            //if (mailSettings != null)
+            //{
+            //    try
+            //    {
+            //        SmtpClient smtp = new SmtpClient();
+            //        smtp.Host = mailSettings.Smtp.Network.Host;
+            //        smtp.Port = mailSettings.Smtp.Network.Port;
+            //        smtp.Credentials = new System.Net.NetworkCredential(mailSettings.Smtp.Network.UserName
+            //                                                           , mailSettings.Smtp.Network.Password);
+            //        smtp.EnableSsl = false;
+            //        smtp.Send(msg);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        throw new Exception("Error Infomation ", ex);
+            //    }
+            //}
+
+            bool success = false;
+            System.Net.ServicePointManager.ServerCertificateValidationCallback =
+                delegate(object s, System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+                { return true; };
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.UseDefaultCredentials = true;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("eakkaphol.s@gmail.com", "eak123**");
+            smtp.Port = 587;
+
+
+            try
+            {
+                smtp.Send(msg);
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+            return success;
+
+        }
+
+
+    }
+}
---
+++ Report.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Text;
+using System.Net.Mail;
+using System.IO;
+using CrystalDecisions.Shared;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Web;
+using Automate.Utilities;
+
+
+
+public partial class Report : System.Web.UI.Page
+{
+    ReportDocument cryRpt = new ReportDocument();
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        showreport();
+
+        //mail();
+        //pdf1();
+        
+    }
+
+    private void showreport()
+    {
+        cryRpt.Load(Server.MapPath("~/Report/Report1.rpt"));
+        cryRpt.SetDatabaseLogon("btpconline", "admin@local", "TSWCLB-DB-SRV", "TSWDATA_ClientCustom");
+        CrystalReportViewer1.ReportSource = cryRpt;
+
+
+        print();
+    }
+
+    private void mail()
+    {
+       
+
+
+
+        MailMessage Msg = new MailMessage();
+        Msg.Subject = "testemail";
+        Msg.BodyEncoding = Encoding.GetEncoding("UTF-8");
+        Msg.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+        Msg.From = new MailAddress("noreply@angsanavacationclub.com", "Angsana Vacation Club");
+        Msg.To.Add(new MailAddress("eakkaphols@lagunaphuket.com", "eakkaphol"));
+      
+
+        Msg.Body = "Quickly please!!!!";
+        Msg.IsBodyHtml = true;
+        ReportDocument rpt = new ReportDocument();
+        rpt.Load(Server.MapPath("~/Report/Report2.rpt"));
+        Msg.Attachments.Add(new Attachment(rpt.ExportToStream(ExportFormatType.PortableDocFormat),  "CFMLetter.pdf"));
+        //System.IO.Stream oStream = null;
+        //byte[] byteArray = null;
+        //oStream = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+        //byteArray = new byte[oStream.Length];
+        //oStream.Read(byteArray, 0, Convert.ToInt32(oStream.Length - 1));
+        //Response.ClearContent();
+       // Response.ClearHeaders();
+       // Response.ContentType = "application/pdf";
+       // Response.BinaryWrite(byteArray);
+        //Response.Flush();
+        //Response.Close();
+
+        //oStream = (MemoryStream)rpt.ExportToStream(ExportFormatType.PortableDocFormat);
+
+       // Response.ContentType = "application/pdf";
+        //Msg.Attachments.Add(new Attachment(oStream, "-CFMLetter.pdf"));
+
+       
+        string factsheet = Server.MapPath("~/pdf/test.pdf");
+        Attachment attachment = new Attachment(factsheet);
+        attachment.Name = "Factsheet.pdf";
+        Msg.Attachments.Add(attachment);
+        Helpers.SendHTMLMail(Msg);
+    }
+
+    private void pdf1()
+    {
+        ReportDocument rpt = new ReportDocument();
+        rpt.Load(Server.MapPath("~/Report/Report2.rpt"));
+        System.IO.Stream oStream = null;
+        byte[] byteArray = null;
+        oStream = rpt.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+        byteArray = new byte[oStream.Length];
+        oStream.Read(byteArray, 0, Convert.ToInt32(oStream.Length - 1));
+        Response.ClearContent();
+        Response.ClearHeaders();
+        Response.ContentType = "application/pdf";
+        Response.BinaryWrite(byteArray);
+        Response.Flush();
+        Response.Close();
+        rpt.Close();
+        rpt.Dispose();
+    }
+
+    private void print()
+    {
+        //CrystalReportViewer1.ReportSource = Session["ReportSource"];
+        int exportFormatFlags = (int)(ViewerExportFormats.PdfFormat |
+                                ViewerExportFormats.ExcelFormat |
+                                ViewerExportFormats.ExcelRecordFormat |
+                                ViewerExportFormats.XLSXFormat);
+        CrystalReportViewer1.AllowedExportFormats = exportFormatFlags;
+        CrystalReportViewer1.Visible = true;
+
+    }
+    protected void Page_UnLoad(object sender, EventArgs e)
+    {
+        CrystalReportViewer1.Dispose();
+        CrystalReportViewer1 = null;
+        cryRpt.Close(); // I can't remember if this is part of the reportDucment class
+        cryRpt.Dispose();
+        cryRpt = null;
+        GC.Collect(); // I forgot this line
+    }
+
+
+}
